45ec02c2df97e591241b9f428aee0cf53219d456
Nils Strand <nstrand@uchicago.edu>
2025-06-18 23:22:23 -0500

    rewrite tt sample function

--- tt_aca.jl
+++ tt_aca.jl
@@ -6,7 +6,7 @@ using MPI
 # Struct holding metadata used for ACA calculations
 mutable struct ResFunc{T, N}
     # Function to be approximated
-    f
+    
     # Number of dimensions
     ndims::Int64
     # Position of the current unfolding (ranges from 0 to ndims - 1)
@@ -437,29 +437,24 @@ function sample_from_tt(F::ResFunc{T, N}, normconst::T) where {T, N}
     npivots = [length(F.I[i]) for i in 2:order]
     sample = Vector{T}(undef, order)
 
-    # Initial marginal over x₁
-    weights1 = zeros(npivots[1])
-    for j in 1:npivots[1]
-        f(x) = F.f((x, F.J[2][j]...)...)
-        weights1[j] = quadgk(f, F.domain[1]...)[1]
-    end
-    weights1 ./= normconst  # Normalize to get p(x₁)
+    rel_tol = 1.0e-3
 
-    # Construct interpolant or CDF over x₁ domain using weights1
+    # CDF for x₁
     function cdf1(x)
         cdfval = 0.0
         for j in 1:npivots[1]
-            f(x) = F.f((x, F.J[2][j]...)...)
+            f = x_ -> F.f((x_, F.J[2][j]...)...)
             val, _ = quadgk(f, F.domain[1][1], x)
             cdfval += val
         end
         return cdfval / normconst
     end
 
-    # Inverse CDF sampling for x₁ via binary search
+    # Sample x₁ using binary search
     u = rand()
     a, b = F.domain[1]
-    for _ in 1:50  # binary search loop
+    abs_tol = rel_tol * abs(b)
+    while b - a > abs_tol
         mid = (a + b) / 2
         if cdf1(mid) < u
             a = mid
@@ -468,45 +463,39 @@ function sample_from_tt(F::ResFunc{T, N}, normconst::T) where {T, N}
         end
     end
     sample[1] = (a + b) / 2
-    cond_weight = weights1
 
     # Recursively sample x₂ through x_d conditionally
     for i in 2:order
-        ni_prev = i == 2 ? 1 : npivots[i - 2]
         ni = npivots[i - 1]
         cond = zeros(ni)
+
         for j in 1:ni
-            function f(x)
-                if i < order
-                    return F.f((sample[1:i-1]..., x, F.J[i+1][j]...)...)
-                else
-                    return F.f((sample[1:i-1]..., x)...)
-                end
+            f = if i < order
+                x -> F.f((sample[1:i-1]..., x, F.J[i+1][j]...)...)
+            else
+                x -> F.f((sample[1:i-1]..., x)...)
             end
             cond[j] = quadgk(f, F.domain[i]...)[1]
         end
         cond ./= sum(cond)
 
-        # Conditional CDF
+        # CDF for xᵢ given x₁:ᵢ₋₁
         function cdfi(x)
             cdfval = 0.0
             for j in 1:ni
-                function f(x)
-                    if i < order
-                        return F.f((sample[1:i-1]..., x, F.J[i+1][j]...)...)
-                    else
-                        return F.f((sample[1:i-1]..., x)...)
-                    end
+                fcond = if i < order
+                    x_ -> F.f((sample[1:i-1]..., x_, F.J[i+1][j]...)...)
+                else
+                    x_ -> F.f((sample[1:i-1]..., x_)...)
                 end
-                val, _ = quadgk(f, F.domain[i][1], x)
+                val, _ = quadgk(fcond, F.domain[i][1], x)
                 cdfval += val
             end
             return cdfval / sum(cond)
         end
 
-        # Inverse CDF for current dimension
+        # Inverse CDF sampling
         u = rand()
-        rel_tol = 1.0e-3
         a, b = F.domain[i]
         abs_tol = rel_tol * abs(b)
         while b - a > abs_tol
