f36849168922318d8725130064c31fe74f914060
Nils Strand <nstrand@uchicago.edu>
2025-06-24 12:19:22 -0500

    something wrong with offset?

--- tt_aca.jl
+++ tt_aca.jl
@@ -28,7 +28,7 @@ mutable struct ResFunc{T, N}
 end
 
 function expnegf(F::ResFunc{T, N}, elements::T...) where {T, N}
-    println("$elements $(F.f(elements...)) $(exp(F.offset - F.f(elements...)))")
+    println("$elements $(F.offset) $(F.f(elements...)) $(exp(F.offset - F.f(elements...)))")
     return exp(F.offset - F.f(elements...))
 end
 
