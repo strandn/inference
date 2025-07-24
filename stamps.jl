129b71ef0c3cfe2e16a2d139e98b7fba056050de
Nils Strand <nstrand@uchicago.edu>
2025-07-24 13:49:59 -0500

    minor

--- stamps.jl
+++ stamps.jl
@@ -14,7 +14,7 @@ function hidalgo_like(x...)
     ps = [MvNormal(μ, Σ) for μ in centers]
 
     # Evaluate density (unnormalized)
-    return 10.0 - log(sum(pdf(p, [elt for elt in x]) for p in ps))
+    return 30.0 - log(sum(pdf(p, [elt for elt in x]) for p in ps))
 end
 
 function aca_stamps()
