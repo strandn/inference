d515d69cdeaa8223fa9b59cd778b309ce3da0253
Nils Strand <nstrand@uchicago.edu>
2025-08-01 16:37:01 -0500

    typo

--- stamps_vanilla_multiple.jl
+++ stamps_vanilla_multiple.jl
@@ -48,7 +48,7 @@ function aca_stamps()
         cov0 = eval(Meta.parse(readline(file)))
     end
 
-    mu, cov = mcmc_mean_cov_parallel(hidalgo_like; domain=dom, comm=mpi_comm, nchains=n_chains, nsamples=n_samples, proposal_std=jump_width, proposal_std=jump_width, periodicity=fill(false, d))
+    mu, cov = mcmc_mean_cov_parallel(hidalgo_like; domain=dom, comm=mpi_comm, nchains=n_chains, nsamples=n_samples, periodicity=fill(false, d))
     if mpi_rank == 0
         println(mu)
         display(cov)
