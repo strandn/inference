5e3560b59bc543649a6cf9d676785e0d16afe3ee
Nils Strand <nstrand@uchicago.edu>
2025-08-03 15:33:03 -0500

    stats from dmrg

--- dmrg_stamps.jl
+++ dmrg_stamps.jl
@@ -19,7 +19,7 @@ function hidalgo_like(x...)
     return 10.0 - log(sum(pdf(p, [elt for elt in x]) for p in ps))
 end
 
-function dmrg_repressilator()
+function dmrg_stamps()
     m1_true = 65.0
     m2_true = 85.0
     m3_true = 115.0
@@ -69,13 +69,109 @@ function dmrg_repressilator()
 
     posterior(x...) = exp(offset - hidalgo_like(x...))
     A = ODEArray(posterior, grid)
-    dmrg_cross(A, maxr, cutoff, tol, maxiter)
+    psi = dmrg_cross(A, maxr, cutoff, tol, maxiter)
+
+    sites = siteinds(psi)
+    oneslist = [ITensor(ones(nbins), sites[i]) for i in 1:d]
+    norm = psi[1] * oneslist[1]
+    for i in 2:d
+        norm *= psi[i] * oneslist[i]
+    end
+
+    println(offset - log(norm[]))
+
+    for pos in 1:d-1
+        Lenv = undef
+        Renv = undef
+        if pos != 1
+            Lenv = psi[1] * oneslist[1]
+            for i in 2:pos-1
+                Lenv *= psi[i] * oneslist[i]
+            end
+        end
+        if pos != d - 1
+            Renv = psi[d] * oneslist[d]
+            for i in d-1:-1:pos+2
+                Renv *= psi[i] * oneslist[i]
+            end
+        end
+        result = undef
+        if pos == 1
+            result = psi[1] * psi[2] * Renv
+        elseif pos + 1 == d
+            result = Lenv * psi[d - 1] * psi[d]
+        else
+            result = Lenv * psi[pos] * psi[pos + 1] * Renv
+        end
+        result /= norm
+        open("dmrg_stamps_marginal_$pos.txt", "w") do file
+            for i in 1:nbins
+                for j in 1:nbins
+                    write(file, "$(grid[pos][i]) $(grid[pos + 1][j]) $(result[sites[pos] => i, sites[pos + 1] => j])\n")
+                end
+            end
+        end
+    end
+
+    vec1list = [ITensor(grid[i][1:nbins], sites[i]) for i in 1:d]
+    meanlist = zeros(d)
+    for i in 1:d
+    mean = psi[1] * (i == 1 ? vec1list[1] : oneslist[1])
+        for k in 2:d
+            mean *= psi[i] * (i == k ? vec1list[k] : oneslist[k])
+        end
+        meanlist[i] = mean[]
+    end
+    println(meanlist)
+
+    cov0 = undef
+    open("stamps0cov.txt", "r") do file
+        cov0 = eval(Meta.parse(readline(file)))
+    end
+
+    vec2list = [ITensor(grid[i][1:nbins] .- meanlist[i], sites[i]) for i in 1:d]
+    vec22list = [ITensor((grid[i][1:nbins] .- meanlist[i]).^2, sites[i]) for i in 1:d]
+    varlist = zeros(d, d)
+    for i in 1:d
+        for j in i:d
+            var = psi[1]
+            if i == 1
+                if i == j
+                    var *= vec22list[1]
+                else
+                    var *= vec2list[1]
+                end
+            else
+                var *= oneslist[1]
+            end
+            for k in 2:d
+                var *= psi[i]
+                if i == k || j == k
+                    if i == j
+                        var *= vec22list[k]
+                    else
+                        var *= vec2list[k]
+                    end
+                else
+                    var *= oneslist[k]
+                end
+            end
+            varlist[i, j] = varlist[j, i] = var[]
+        end
+    end
+    display(varlist)
+    println(LinearAlgebra.norm(varlist - cov0) / LinearAlgebra.norm(cov0))
+    flush(stdout)
 end
 
-d = 8
+d = 9
 maxr = 100
 cutoff = 1.0e-4
 tol = 0.01
 maxiter = 10
 
-dmrg_repressilator()
+start_time = time()
+dmrg_stamps()
+end_time = time()
+elapsed_time = end_time - start_time
+println("Elapsed time: $elapsed_time seconds")
