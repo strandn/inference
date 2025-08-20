8e7e3de8a8cd58417e15cb60b87d6c4e81a7e731
Nils Strand <nstrand@uchicago.edu>
2025-08-20 11:47:28 -0500

    oops

--- tt_repressilator_sample.jl
+++ tt_repressilator_sample.jl
@@ -123,6 +123,7 @@ function tt_repressilator()
     close(f)
 
     sites = siteinds(psi)
+    println(sites)
     oneslist = [ITensor(ones(dim(sites[i])), sites[i]) for i in 1:d]
     norm = psi[1] * oneslist[1]
     for i in 2:d
