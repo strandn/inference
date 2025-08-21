3288c81a211c9b04cd5c42f0e12a8663bfc12cc4
Nils Strand <nstrand@uchicago.edu>
2025-08-21 12:47:52 -0500

    oops

--- 50bins/tt_repressilator.jl
+++ 50bins/tt_repressilator.jl
@@ -321,12 +321,10 @@ function tt_repressilator()
                 end
 
                 if abs(cdfi[] / normi[] - u) < abs(cdfi_b[] / normi[] - u)
-                    # sample[count] = grid_full[count][a]
-                    sample[count] = grid[count][a]
+                    sample[count] = grid_full[count][a]
                     sampleidx[count] = a
                 else
-                    # sample[count] = grid_full[count][b]
-                    sample[count] = grid[count][b]
+                    sample[count] = grid_full[count][b]
                     sampleidx[count] = b
                 end
             end
