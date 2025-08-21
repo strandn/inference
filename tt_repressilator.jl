5566761f40340f48c9555b4558be76c89c740d15
Nils Strand <nstrand@midway3-login3.rcc.local>
2025-08-21 18:35:30 -0500

    ttsvd results

--- tt_repressilator.jl
+++ tt_repressilator.jl
@@ -88,7 +88,7 @@ function tt_repressilator()
     #         samples[i, :] = sample
     #     end
     # end
-    samples = readdlm("tt_repressilator_samples.txt")
+    samples = readdlm("tt_repressilator_fine_samples.txt")
     # samples = readdlm("tt_repressilator_coarse_samples.txt")
     R = kmeans(samples', 3)
     borders = []
