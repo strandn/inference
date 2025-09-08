5e3aa6ca05b5115c08fc110fd7404fe961825e4b
Nils Strand <nstrand@midway3-login3.rcc.local>
2025-09-08 18:47:22 -0500

    samples from second aca scheme

--- 18/repressilator.jl
+++ 18/repressilator.jl
@@ -89,8 +89,8 @@ mpi_rank = MPI.Comm_rank(mpi_comm)
 mpi_size = MPI.Comm_size(mpi_comm)
 
 d = 8
-maxr = 10
-n_chains = 20
+maxr = 50
+n_chains = 100
 n_samples = 2000
 jump_width = 0.01
 cutoff = 1.0e-6
