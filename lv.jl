725a61e17369520f7add0c4b62648710d922f76a
Nils Strand <nstrand@uchicago.edu>
2025-09-12 13:24:35 -0500

    clean up

--- lv.jl
+++ lv.jl
@@ -74,10 +74,6 @@ function aca_lv()
             write(file, "$IJ\n")
             write(file, "$(F.offset)\n")
         end
-        norm, _, _ = compute_norm(F)
-        println("norm = $norm")
-        println(F.offset - log(norm))
-        flush(stdout)
     end
 end
 
