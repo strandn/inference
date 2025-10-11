f49a1b7660a37d04da0d7dbb7d73c56d6a9ee56f
Nils Strand <nstrand@uchicago.edu>
2025-10-11 12:12:19 +0200

    another slight adjustment

--- tt_repressilator.jl
+++ tt_repressilator.jl
@@ -92,13 +92,13 @@ function tt_repressilator()
                 idx = findall(x -> x == j, assignments(R))
                 avg = mean(samples[idx, i])
                 sd = std(samples[idx, i])
-                push!(clusterborders, (avg - 8 * sd, avg + 8 * sd))
+                push!(clusterborders, (avg - 9 * sd, avg + 9 * sd))
             end
             push!(borders, clusterborders)
         else
             avg = mean(samples[:, i])
             sd = std(samples[:, i])
-            push!(borders, [(avg - 8 * sd, avg + 8 * sd)])
+            push!(borders, [(avg - 9 * sd, avg + 9 * sd)])
         end
         println(borders[i])
     end
