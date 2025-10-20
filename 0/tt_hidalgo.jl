c17442ff6ba7be96230cbf1de5e39e6b4d3c1dc1
Nils Strand <nstrand@uchicago.edu>
2025-10-20 18:37:35 -0500

    oops

--- 0/tt_hidalgo.jl
+++ 0/tt_hidalgo.jl
@@ -372,7 +372,7 @@ function tt_hidalgo()
                 end
             end
 
-            write(file, "$(sample[1]) $(sample[2]) $(sample[3]) $(sample[4]) $(sample[5]) $(sample[6]) $(sample[7]) $(sample[8])\n")
+            write(file, "$(sample[1]) $(sample[2]) $(sample[3]) $(sample[4]) $(sample[5]) $(sample[6]) $(sample[7]) $(sample[8]) $(sample[9])\n")
         end
     end
 end
